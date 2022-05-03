4a9278dd24efb99704b738c7ba504c14d393cedb
drex2-0 <84222894+drex2-0@users.noreply.github.com>
2022-05-03 16:52:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
+song="";
+function preload()
+{
+    song=loadSound("music.mp3");
+}
 function setup()
 {
     canvas=createCanvas(600,500);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    posenet=ml5.poseNet(video,modelloaded);
+    posenet.on("pose",gotposes);
+
+}
+function modelloaded()
+{
+    console.log("Posenet is started")
+}
+function draw()
+{
+    image(video,0,0,600,500);
+}
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function gotposes(results)
+{
+    console.log(results);
 }
